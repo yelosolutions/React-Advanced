e695f2530e8848d6b3eafcf98542a10ec6258827
jay <ronokamau@gmail.com>
2023-05-10 23:03:10 +0300

    Advanced React Intro and a few deletions

---
+++ second-app/src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+    {
+        img : 'https://m.media-amazon.com/images/I/51ZLFV0ZBwL._SX375_BO1,204,203,200_.jpg',
+        title : 'The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collectors Edition',
+        author : 'Piggyback'
+    },
+    {
+        img : 'https://m.media-amazon.com/images/I/51ZLFV0ZBwL._SX375_BO1,204,203,200_.jpg',
+        title : 'The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collectors Edition',
+        author : 'Piggyback'
+    },
+    {
+        img : 'https://m.media-amazon.com/images/I/51ZLFV0ZBwL._SX375_BO1,204,203,200_.jpg',
+        title : 'The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collectors Edition',
+        author : 'Piggyback'
+    },
+    {
+        img : 'https://m.media-amazon.com/images/I/51ZLFV0ZBwL._SX375_BO1,204,203,200_.jpg',
+        title : 'The Legend of Zelda™: Tears of the Kingdom – The Complete Official Guide: Collectors Edition',
+        author : 'Piggyback'
+    }
+    
+]
