08a0ccc07c9ac0749637682f4e091cc696806056
jay <ronokamau@gmail.com>
2023-05-11 23:55:35 +0300

    update on useState hook

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-//import Setup from './tutorial/1-useState/setup/2-useState-basics';
-import Count from './tutorial/1-useState/setup/useState-try';
+import Setup from './tutorial/1-useState/setup/2-useState-basics';
+//import Count from './tutorial/1-useState/setup/useState-try';
 //import Final from './tutorial/1-useState/final/2-useState-basics';
 
 function App() {
   return (
     <div className='container'>
-      {/* <Setup/> */}
-      <Count/>
+      <Setup/>
+      {/* <Count/> */}
       {/* <Final/> */}
     </div>
   )
