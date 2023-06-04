85fccc216ecbc1c3d4a2864963b060462976d8ea
jay <ronokamau@gmail.com>
2023-06-05 00:06:33 +0300

    tailwindcss setup and controlled inputs update

--- second-app/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ second-app/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -15,28 +15,42 @@ const ControlledInputs = () => {
     const [email, setEmail] = useState('');
 
 
-    const submitForm = (e) => {
+
+    //create a list of users
+    const [people, setPeople] = useState([]);
+
+
+    const handelSubmit = (e) => {
         e.preventDefault();
-        console.log(firstName, email);
+        if(firstName && email){
+            const person = { id: new Date().getTime().toString(),
+                firstName, email
+            };
+            setPeople((people) => [...people, person]);
+            setFirstName('');
+            setEmail('');
+        }
+        
     };
 
-    return <> 
+    return( 
     <article>
-        <form className="form" onSubmit={submitForm}>
+        <form className="form" onSubmit={handelSubmit}>
             <h2>Login</h2>
             <div className="form-control" >
-                <label htmlFor="firstName">Name : </label>
-                <input 
+                <label className='font-bold' htmlFor="firstName">Name </label>
+                <input className='bg-gray-300 rounded-md p-1 w-full' 
                     type="text"  
                     id='firstName' 
                     name='firstName'
                     value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
                 />
-            </div>
-            <div className="form-control">
-                <label htmlFor="email">Email:</label>
-                <input 
+                <label className='font-bold' 
+                    htmlFor="email"
+                >Email
+                </label>
+                <input className='bg-gray-300 rounded-md p-1 w-full'
                     type="text"  
                     id='email' 
                     name='email'
@@ -46,7 +60,16 @@ const ControlledInputs = () => {
             </div>
             <button type='submit' >Add Person</button>
         </form>
+
+        {people.map((person) => {
+            const {id, firstName, email} = person;
+            return <div key={id} className='flex bg-white mb-3 items-center text-center gap-16 rounded-md p-2'>
+                <h3 className='left-2 font-bold'>{firstName}</h3>
+                <p className='font-semibold text-black text-lg'>{email}</p>
+            </div>
+        })}
+        <button onClick={() => setPeople([])} className='w-12 p-1 bg-slate-800 text-white rounded-md'> Clear</button>
     </article>
-    </>
+    )
 }
 export default ControlledInputs;
