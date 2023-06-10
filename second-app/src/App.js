3fc3a2623dd57585b0acbcecdae633b53b5058ad
jay <ronokamau@gmail.com>
2023-06-10 23:56:12 +0300

    more on context api

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-// import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
-import Final from './tutorial/9-custom-hooks/setup/1-fetch-example';
+import Setup from './tutorial/8-useContext/setup/try-context-api';
+// import Final from './tutorial/9-custom-hooks/setup/1-fetch-example';
 
 
 function App() {
 	return (
 		<div className='container'>
-		{/* <Setup/> */}
-		<Final/>
+		<Setup/>
+		{/* <Final/> */}
 		</div>
 	)
 }
---
+++ second-app/src/tutorial/8-useContext/setup/try-context-api.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//useContext hook - 
+
+const TryContextApi = () => {
+    return (
+        <h3>TryContextApi</h3>
+    );
+};
+
+export default TryContextApi;
