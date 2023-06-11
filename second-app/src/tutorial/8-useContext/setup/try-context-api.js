a8116ec75b4364204e33004efab527a7309bba43
jay <ronokamau@gmail.com>
2023-06-11 03:17:45 +0300

    more on useContext

--- second-app/src/tutorial/8-useContext/setup/try-context-api.js
+++ second-app/src/tutorial/8-useContext/setup/try-context-api.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {data} from '../../../data';
 
-//useContext hook - 
-
+/**
+ * useContext hook - Allow inheritance of data(removePerson function)
+ *  down the heirarchy
+*/
 const TryContextApi = () => {
+    const [people, setPeople] = useState(data);
+
+    const removePerson = (id) => {
+        const newPeople = people.filter((person) => person.id !== id );
+        return setPeople(newPeople);
+    };
+
     return (
+        <>
         <h3>TryContextApi</h3>
+        <List people={people}/>
+        </>
     );
+};
+
+
+
+const List = ({people}) => {
+    return <section>
+        {people.map((person) => {
+            return <SinglePerson key={person.id} name={person.name}/>
+        })}
+        
+    </section>
+};
+
+const SinglePerson = ({id, name}) => {
+    return <div className="item">
+        <h3>{name}</h3>
+        <button ></button>
+    </div>
+
 };
 
 export default TryContextApi;
