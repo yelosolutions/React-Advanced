7f632323e495cba6e090044afebc49731342a842
jay <ronokamau@gmail.com>
2023-06-21 19:58:16 +0300

    getting started on Prop Types

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import Example from './tutorial/9-custom-hooks/trial/example';
 // import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
-// import Final from './tutorial/9-custom-hooks/final/1-fetch-example';
+import Final from './tutorial/10-prop-types/final';
 
 
 function App() {
 	return (
 		<div className='container'>
-			<Example/>
 		{/* <Setup/> */}
-		{/* <Final/> */}
+		<Final/>
 		</div>
 	)
 };
